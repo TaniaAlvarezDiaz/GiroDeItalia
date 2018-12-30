5505b3388d37a26b645ff717f0ba4ec0e0691196
Pedro <pedrofernandezalvarez6@gmail.com>
2018-12-30 15:17:41 +0100

    Zoom, Firebase y Glide funcionando

--- app/src/main/java/es/uniovi/sdm/alvfer/girodeitalia/vista/RecorridoActivity.java
+++ app/src/main/java/es/uniovi/sdm/alvfer/girodeitalia/vista/RecorridoActivity.java
@@ -2,17 +2,18 @@ package es.uniovi.sdm.alvfer.girodeitalia.vista;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DecodeFormat;
-import com.bumptech.glide.request.RequestOptions;
 import com.github.chrisbanes.photoview.PhotoView;
-import com.squareup.picasso.Picasso;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import es.uniovi.sdm.alvfer.girodeitalia.R;
 
 public class RecorridoActivity extends AppCompatActivity {
+
+    private FirebaseStorage firebaseStorage;
+    private StorageReference mapaRecorridoImagenesStorageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +21,19 @@ public class RecorridoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recorrido);
 
         PhotoView photoView = (PhotoView) findViewById(R.id.ivRecorrido);
+
         //Comentar si se usa Glide
         //photoView.setImageResource(R.drawable.mapa_recorrido);
 
         //Glide.with(this).load(R.drawable.mapa_recorrido).into(photoView);
-        Picasso.get().load(R.drawable.mapa_recorrido).into(photoView);
+        //Picasso.get().load(R.drawable.mapa_recorrido).into(photoView);
+
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        mapaRecorridoImagenesStorageReference = firebaseStorage.getReference().child
+                ("MapaRecorrido");
+        StorageReference photoRef = mapaRecorridoImagenesStorageReference.child("MapaRecorrido.png");
+        Glide.with(this).load(photoRef).into(photoView);
+
     }
 }
