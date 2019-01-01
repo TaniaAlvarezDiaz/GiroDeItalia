d755b2a67cdaedb6a7656c5f2ee8538df383d0ad
Pedro <pedrofernandezalvarez6@gmail.com>
2019-01-01 19:10:07 +0100

    Algunos monumentos reales añadidos

--- app/src/main/java/es/uniovi/sdm/alvfer/girodeitalia/datos/utilidades/FirebaseUtilidades.java
+++ app/src/main/java/es/uniovi/sdm/alvfer/girodeitalia/datos/utilidades/FirebaseUtilidades.java
@@ -46,33 +46,53 @@ public class FirebaseUtilidades {
 
                 // Crear los elementos del patrimonio
                 ArrayList<ElementoPatrimonio> elementosPatrimonio = new ArrayList<>();
-                /*elementosPatrimonio.add(new ElementoPatrimonio("Basílica de San Petronio", "La " +
+                elementosPatrimonio.add(new ElementoPatrimonio("Basílica de San Petronio", "La " +
                         "Basílica " +
-                        "de San Petronio es la iglesia más antigua y grande de Bolonia, y una de " +
+                        "de San Petronio es la iglesia más antigua y grande de Bologna, y una de " +
                         "las más " +
-                        "grandes del mundo.", 1, "Bolonia", "BasilicaDeSanPetronio.jpg",
-                        "historico"));
+                        "grandes del mundo.", 1, "Bologna", "BasilicaDeSanPetronio.jpg",
+                        "historico")); // 44.493204, 11.343118
                 elementosPatrimonio.add(new ElementoPatrimonio("Torres de los Asinelli y " +
                         "Garisenda", "En " +
-                        "el pasado la ciudad de Bolonia tenía varias torres con funciones de " +
+                        "el pasado la ciudad de Bologna tenía varias torres con funciones de " +
                         "defensa y " +
                         "señalización. Hoy en día solo quedan unas veinte, de las cuales las más " +
                         "famosas " +
-                        "son estas dos.", 1, "Bolonia", "TorresDeLosAsinelliYGarisenda.jpg",
-                        "historico"));*/
+                        "son estas dos.", 1, "Bologna", "TorresDeLosAsinelliYGarisenda.jpg",
+                        "historico")); // 44.494236, 11.346732
+                elementosPatrimonio.add(new ElementoPatrimonio("Santuario de San Luca", "El " +
+                        "santuario de Nuestra Señora de San Luca, ubicado sobre la cima del Colle" +
+                        " della Guardia, está dedicado al culto católico mariano y es la meta de " +
+                        "una gran cantidad de peregrinos todos los años.", 1, "Bologna",
+                        "SantuarioDeSanLuca.jpg", "historico")); // 44.479322, 11.298053
+                elementosPatrimonio.add(new ElementoPatrimonio("Pinacoteca Nacional de Bologna",
+                        "La Pinacoteca Nacional de Bologna es uno de los museos más importantes y" +
+                                " relevantes de Italia. Entre sus obras más importantes está la " +
+                                "colección de pinturas de los Carracci y sus seguidores ", 1,
+                        "Bologna",
+                        "PinacotecaNacionalDeBologna.jpg", "cultural")); // 44.497952, 11.353498
+                elementosPatrimonio.add(new ElementoPatrimonio("Plaza de los Leones", "Esta plaza" +
+                        " es la más importante de la ciudad e Empoli. Debe su nombre a la fuente " +
+                        "que se encuentra en su centro, la cual está adornada con leones, data de" +
+                        " 1827 y es obra de Luigi Pampaloni.", 2, "Empoli", "PlazaDeLosLeones" +
+                        ".jpg", "cultural"));
+
 
                 for (int i = 0; i < 40; i++) {
-                    elementosPatrimonio.add(new ElementoPatrimonio("EPCultural" + i, "Una descripcion",
+                    elementosPatrimonio.add(new ElementoPatrimonio("EPCultural" + i, "Una " +
+                            "descripcion",
                             1, "Un lugar", "TorresDeLosAsinelliYGarisenda.jpg", "cultural"));
                 }
 
                 for (int i = 0; i < 40; i++) {
-                    elementosPatrimonio.add(new ElementoPatrimonio("EPGeografico" + i, "Una descripcion",
+                    elementosPatrimonio.add(new ElementoPatrimonio("EPGeografico" + i, "Una " +
+                            "descripcion",
                             2, "Un lugar", "TorresDeLosAsinelliYGarisenda.jpg", "geografico"));
                 }
 
                 for (int i = 0; i < 40; i++) {
-                    elementosPatrimonio.add(new ElementoPatrimonio("EPHistorico" + i, "Una descripcion",
+                    elementosPatrimonio.add(new ElementoPatrimonio("EPHistorico" + i, "Una " +
+                            "descripcion",
                             3, "Un lugar", "TorresDeLosAsinelliYGarisenda.jpg", "historico"));
                 }
                 /*
@@ -132,17 +152,20 @@ public class FirebaseUtilidades {
                 // https://es.wikipedia.org/wiki/Giro_de_Italia_2019#Clasificaciones_finales
                 // Hay 3 contrarrelojes individuales, en las etapas 1, 9 y 21
                 ArrayList<Etapa> etapas = new ArrayList<>();
-                etapas.add(new Etapa("Bologna", "Bologna (San Luca)", "Contrarreloj individual", 1, 8, "11-Mayo"));
+                etapas.add(new Etapa("Bologna", "Bologna (San Luca)", "Contrarreloj individual",
+                        1, 8, "11-Mayo"));
                 etapas.add(new Etapa("Bologna", "Fucecchio", "Media montaña", 2, 200, "12-Mayo"));
                 etapas.add(new Etapa("Vinci", "Orbetello", "Media montaña", 3, 219, "13-Mayo"));
                 etapas.add(new Etapa("Orbetello", "Frascati", "Media montaña", 4, 228, "14-Mayo"));
                 etapas.add(new Etapa("Frascati", "Terracina", "Llana", 5, 140, "15-Mayo"));
-                etapas.add(new Etapa("Cassino", "San Giovanni Rotondo", "Media montaña", 6, 233, "16-Mayo"));
+                etapas.add(new Etapa("Cassino", "San Giovanni Rotondo", "Media montaña", 6, 233,
+                        "16-Mayo"));
                 etapas.add(new Etapa("Vasto", "L'Aquila", "Media montaña", 7, 180, "17-Mayo"));
-                etapas.add(new Etapa("Tortoreto Lido", "Pesaro", "Media montaña", 8, 235, "18-Mayo"));
-                etapas.add(new Etapa("Riccione", "San Marino", "Contrarreloj individual", 9, 35, "19-Mayo"));
+                etapas.add(new Etapa("Tortoreto Lido", "Pesaro", "Media montaña", 8, 235,
+                        "18-Mayo"));
+                etapas.add(new Etapa("Riccione", "San Marino", "Contrarreloj individual", 9, 35,
+                        "19-Mayo"));
                 etapas.add(new Etapa("Ravenna", "Modena", "Llana", 10, 147, "21-Mayo"));
-
 
 
                 Log.d("FIREBASE", "Etapas a insertar: " + etapas
